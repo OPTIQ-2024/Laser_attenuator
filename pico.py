4b8bb4fd0d35c2de41e91f714280729450f8df03
Abigail Trouillet <abigail.trouillet@free.fr>
2024-12-06 15:33:24 +0100

    Supprimer des fichiers inutiles

--- pico.py
+++
@@ -1,5 +0,0 @@
-from ctypes import *
-from picosdk.ps2000a import ps2000a as ps
-import numpy as np
-import matplotlib.pyplot as plt
-from picosdk.functions import adc2mV, assert_pico_ok
--- test.py
+++
@@ -1,2 +0,0 @@
-import ps2000a
-ps = ps2000a.PS2000a()
